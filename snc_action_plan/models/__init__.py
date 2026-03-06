ce0b54337779cfb32c5de8a7a6bd26125ed62b22
bushrasultn <bushra37969@gmail.com>
2026-03-06 10:11:36 +0500

    Delete snc_action_plan directory

--- snc_action_plan/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import target
-from . import achievement
-from . import checklist
-from . import achievement_reports
--- snc_action_plan/wizard/__init__.py
+++
@@ -1,3 +0,0 @@
-from .import region_achievmnt
-from .import territory_achievement
-# from .import target_vs_achieve
